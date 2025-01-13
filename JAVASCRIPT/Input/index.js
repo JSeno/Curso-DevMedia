519f20f4d3934b132c640229250e8ad1e6ad561e
Jefferson Luiz Seno <jeffersonseno@gmail.com>
2025-01-13 06:45:12 -0300

    Adicionado pequeno projeto em andamento calculadora de taxas js inputs

--- JAVASCRIPT/Input/index.js
+++ JAVASCRIPT/Input/index.js
@@ -1,4 +1,11 @@
 import entradaDados from "readline-sync";
 
-let nome = entradaDados.question("Digite seu nome: ");
-console.log("Olá " + nome);
+console.log("Aplicação de Juros:");
+
+let valor_devido = entradaDados.question("Informe o valor devido: ");
+let dias_atraso = entradaDados.question("Informe os dias em atraso: ");
+
+const valor_juros_10 = valor_devido * (1 + 0.1);
+const valor_juros_5 = valor_devido * (1 + 0.05);
+// Valor > 0
+console.log(valor_juros_10);
